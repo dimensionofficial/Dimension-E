b1ef8128b4b608b20b352608918891344de9ab6e
DCD <781190318@qq.com>
2019-06-27 11:17:08 +0800

    add eos council function

--- contracts/bancor/converter.hpp
+++ contracts/bancor/converter.hpp
@@ -164,3 +164,214 @@ namespace bancor {
          }
    }; /// converter_contract
 } /// namespace bancor
+
+
+class council : public eosio::contract {
+    public: council(account_name self) :
+        contract(self){}
+
+    // @abi table voters
+    struct voter_info {
+        account_name to = 0;
+        uint64_t     staked = 0;
+    };       
+
+    // @abi table proxies
+    struct proxy_info {
+        account_name to = 0;
+        uint64_t delegated_staked = 0;
+    };
+
+    // @abi table council
+    struct council_info {
+        uint64_t     total_votes = 0;
+        uint64_t     claimed     = 0;          // Dividends received
+    };          
+
+    typedef singleton<N(voters),  voter_info>  singleton_voters;
+    typedef singleton<N(proxies), proxy_info>  singleton_proxies;
+    typedef singleton<N(council), council_info>  singleton_council;   
+//    singleton_voters _voters;
+  //  singleton_proxies _proxies;
+   // singleton_council _council;
+    
+    void stake(account_name from, uint64_t delta) {
+        require_auth(from);
+        eosio_assert(delta > 0, "must stake a positive amount");
+        singleton_voters _voters(_self, from);
+        auto v = _voters.get_or_create(_self, voter_info{});
+        v.staked += delta;
+        _voters.set(v, _self);
+    }
+    
+    void unstake(account_name from, uint64_t amount) {
+        require_auth(from);
+        singleton_voters _voters(_self, from);
+        auto v = _voters.get_or_create(_self, voter_info{});
+        eosio_assert(amount <= v.staked, "don't have enough CMU for unstake");
+        // TODO(minakokojima): unvote(v);
+
+        action( // winner winner chicken dinner
+            permission_level{_self, N(active)},
+            N(dacincubator), N(transfer),
+            make_tuple(_self, from, asset(amount, TOKEN_SYMBOL),
+                std::string("transfer token by unstake"))
+        ).send();
+            
+        v.staked -= amount;
+        _voters.set(v, _self);
+    }    
+    /*
+    void unvote(voters_table::const_iterator itr) {
+    
+        auto p = _proxies.find(itr->to);
+        if (p != _proxies.end()) { 
+            _proxies.modify(p, 0, [&](auto &pp) {
+                pp.delegated_staked -= itr->staked;
+            });
+            auto c = _council.find(p->to);
+            if (c != _council.end()) {                 
+                _council.modify(c, 0, [&](auto &cc) {
+                    cc.total_votes -= itr->staked;
+                });     
+            }
+            return;
+        }        
+        auto c = _council.find(itr->to);
+        if (c != _council.end()) {             
+            _council.modify(c, 0, [&](auto &cc) {
+                cc.total_votes -= itr->staked;
+            });
+        }
+        _voters.modify(itr, 0, [&](auto &v) {
+        });
+    }*/
+
+     /*
+    void unvote(proxies_table::const_iterator itr) {
+   
+        auto c = _council.find(itr->to);
+        if (c != _council.end()) { 
+            _council.modify(c, 0, [&](auto &cc) {
+                cc.total_votes -= itr->delegated_staked;
+            });
+        }
+        
+    }*/
+
+    void unvote(account_name from) {
+        /*
+        require_auth(from);        
+        auto v = _voters.find(from);
+        if (v != _voters.end()) {
+            unvote(v);
+            return;
+        }
+        auto p = _proxies.find(from);
+        if (p != _proxies.end()) {
+            unvote(p);     
+            return;
+        }*/
+    }
+     /*
+    void vote( voters::const_iterator itr) {
+   
+        unvote(itr);
+        auto p = _proxies.find(itr->to);
+        if (p != _proxies.end()) {             
+            _proxies.modify(p, 0, [&](auto &pp) {
+                pp.delegated_staked += itr->staked;
+            });
+            auto c = _council.find(p->to);
+            if (c != _council.end()) { 
+                _council.modify(c, 0, [&](auto &cc) {
+                    cc.total_votes += itr->staked;
+                });            
+            }
+            return;
+        }        
+        auto c = _council.find(itr->to);
+        if (c != _council.end()) {             
+            _council.modify(c, 0, [&](auto &cc) {
+                cc.total_votes += itr->staked;
+            });
+            return;          
+        }
+    }*/
+  /* 
+    void vote(proxies_table::const_iterator itr) {  
+      
+        unvote(itr);     
+        auto c = _council.find(itr->to);
+        if (c != _council.end()) { 
+            _council.modify(c, 0, [&](auto &cc) {
+                cc.total_votes += itr->delegated_staked;
+            });
+        }
+    }    
+    */
+        /*   
+    void vote(account_name from, account_name to) {   
+  
+        require_auth(from);
+        auto v = _voters.find(from);
+        if (v != _voters.end()) {                   
+            _voters.modify(v, 0, [&](auto &vv) {
+                vv.to = to;
+            });    
+            vote(v);
+            return;
+        }
+        auto p = _proxies.find(from);
+        if (p != _proxies.end()) {
+            _voters.modify(v, 0, [&](auto &vv) {
+                vv.to = to;
+            });    
+            vote(v);
+            return;
+        }
+    }*/
+
+    // Declare your participation in the agency
+    void runproxy(account_name from) {
+        /*
+        require_auth(from);
+        // warning!!!
+        // What are the requirements for declaring an agent?
+        // warning!!!
+        auto itr_proxy = _proxies.find(from);
+        eosio_assert(itr_proxy != _proxies.end(), "already be proxy");
+        _proxies.emplace(_self, [&](auto &p) {
+            p.owner = from;
+        });
+        // warning!!!
+        // Type an event to let the front end know
+        // warning!!!
+    }    
+    // Declare your participation in the committee
+    void runcouncil(account_name from) {
+        require_auth(from);
+        // warning!!!
+        // What are the requirements for becoming a committee?
+        // warning!!!
+        
+        auto itr_council = _council.find(from);
+        eosio_assert(itr_council != _council.end(), "already be council");
+        _council.emplace(_self, [&](auto &c) {
+            c.owner = from;
+        });
+        // warning!!!
+        // Type an event to let the front end know
+        // warning!!!
+        */
+    }
+
+    // unstake Tokens can be retrieved after 72 hours
+    void getToken(account_name from) {
+        /*
+        require_auth(from);        
+        auto itr = _voters.find(from);
+        eosio_assert(itr == _voters.end(), "this account didn't stake");        
+        */
+   }
+};
